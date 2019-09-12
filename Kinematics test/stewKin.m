73dc5c547ff844c8c7d2b88abe332bd51f8f4dbc
Jin Sing Sia <jinsing.sia@gmail.com>
2019-09-11 20:36:12 -0400

    Now supports various platform joint angles

--- Kinematics test/stewKin.m
+++ Kinematics test/stewKin.m
@@ -1,8 +1,5 @@
 clc; close all; clear all;
 % Algorithm taken from: https://www.instructables.com/id/Stewart-Platform/
-% --- Constants ---
-hexAngles = [0 60 120 180 240 300];
-hexAngles = degtorad(hexAngles);
 
 % --- Stewart platform design parameters ---
 % Platform radius [m]
@@ -11,51 +8,61 @@ Rp = 0.1;
 % Base radius [m]
 Rb = 0.12;
 
-% --- Calculate neutral position ---
+% Horn length [m]
+a = 0.01;
+
+% Angles of base plate joints
+baseAngles = [0 60 120 180 240 300];
+baseAngles = degtorad(baseAngles);
+
+% Angles of platform joints
+platAngles = [0 0 120 120 240 240];	% Triangle legs
+% platAngles = [0 60 120 180 240 300];	% Equispaced legs
+platAngles = degtorad(platAngles);
+
+% Position of platform centroid (relative to base centroid) [m]
+T = [0.05, 0, 0.25]';
+
+% Platform angles (x, y, z) [deg]
+Pang = [45, 0, 45];
+
+% --- Calculate rod vectors ---
 % Calculate rod-platform joints (platform coords.)
 P = zeros(3, 6);
-P(1, :) = Rp .* cos(hexAngles);
-P(2, :) = Rp .* sin(hexAngles);
+P(1, :) = Rp .* cos(platAngles);
+P(2, :) = Rp .* sin(platAngles);
 
 % Calculate base-rod joints (base coords.)
 B = zeros(3, 6);
-B(1, :) = Rb .* cos(hexAngles);
-B(2, :) = Rb .* sin(hexAngles);
-
-O = [0 0 0]';
+B(1, :) = Rb .* cos(baseAngles);
+B(2, :) = Rb .* sin(baseAngles);
 
 % --- Define kinematic matrices ---
 ProtB = @(phi, theta, psi) ...
-    [   cos(psi) * cos(theta),     -sin(psi) * cos(phi) + cos(psi) * sin(theta) * sin(phi),    sin(psi) * sin(phi) + cos(psi) * sin(theta) * cos(phi); ...
-        sin(psi) * cos(theta),      cos(psi) * cos(phi) + sin(psi) * sin(theta) * sin(phi),   -cos(psi) * sin(phi) + sin(psi) * sin(theta) * cos(phi); ...
-       -sin(theta)           ,      cos(theta) * sin(phi)                                 ,    cos(theta) * cos(phi)];
+   [cos(psi) * cos(theta),     -sin(psi) * cos(phi) + cos(psi) * sin(theta) * sin(phi),    sin(psi) * sin(phi) + cos(psi) * sin(theta) * cos(phi); ...
+    sin(psi) * cos(theta),      cos(psi) * cos(phi) + sin(psi) * sin(theta) * sin(phi),   -cos(psi) * sin(phi) + sin(psi) * sin(theta) * cos(phi); ...
+   -sin(theta)           ,      cos(theta) * sin(phi)                                 ,    cos(theta) * cos(phi)];
 
-% --- Test ---
-% Position of platform centroid relative to base centroid [m]
-T = [0.05, 0, 0.25]';
-
-% Platform angles, (x, y, z) [deg]
-Pang = [45, 0, 45];
-
-% Do not touch this
+% --- Compute reverse kinematics ---
 Pang = degtorad(Pang);
 L = zeros(3, 6);
+O = [0 0 0]';
 
 % Calculate leg vectors
-for i = 1:6
-    L(:, i) = T + ProtB(Pang(1), Pang(2), Pang(3)) * P(:, i) - B(:, i);
+for ii = 1:6
+    L(:, ii) = T + ProtB(Pang(1), Pang(2), Pang(3)) * P(:, ii) - B(:, ii);
 end
 
 % Transform platform vectors
-for i = 1:6
-   Pxyz(:, i) = ProtB(Pang(1), Pang(2), Pang(3)) * P(:, i); 
+for ii = 1:6
+   Pxyz(:, ii) = ProtB(Pang(1), Pang(2), Pang(3)) * P(:, ii); 
 end
 
-for i = 1:6
-    plotVec(T, Pxyz(:, i) + T, 'r');
+for ii = 1:6
+    plotVec(T, Pxyz(:, ii) + T, 'r');
     hold on;
-    plotVec(O, B(:, i), 'g');
-    plotVec(B(:, i), B(:, i) + L(:, i), 'b');
+    plotVec(O, B(:, ii), 'g');
+    plotVec(B(:, ii), B(:, ii) + L(:, ii), 'b');
 end
 
 xlabel('x [m]');
@@ -64,6 +71,6 @@ zlabel('z [m]');
 
 disp('Linkage lengths:');
 Llen = sqrt(sum(L.^2));
-for i = 1:6
-    disp(['Link ' num2str(i) ': ' num2str(Llen(i)) ' m']);
+for ii = 1:6
+    disp(['Link ' num2str(ii) ': ' num2str(Llen(ii)) ' m']);
 end
