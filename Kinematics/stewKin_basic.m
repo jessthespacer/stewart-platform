8394cfe7e8ffa3a60896c46be90eedfb2dd9b0e0
Jin Sing Sia <jinsing.sia@gmail.com>
2019-09-15 23:49:16 -0400

    Completed full code for reverse kinematics to calculate servo angles. Use `stewKin_basic.m` for streamlined version of code.

--- Kinematics/stewKin_basic.m
+++ Kinematics/stewKin_basic.m
@@ -1,4 +1,4 @@
-clc; close all; clear all;
+close all; clear all;
 % Sources:
 % https://www.instructables.com/id/Stewart-Platform/
 % https://www.xarg.org/paper/inverse-kinematics-of-a-stewart-platform/
@@ -22,19 +22,23 @@ baseAngles = [0 60 120 180 240 300];
 baseAngles = deg2rad(baseAngles);
 
 % Angles of platform joints
-% platAngles = [30 30 150 150 270 270];	% Triangle legs
-platAngles = [0 60 120 180 240 300];	% Equispaced legs
+% platAngles = [30 30 150 150 270 270];	    % Triangle legs
+platAngles = [0 60 120 180 240 300];        % Equispaced legs
 platAngles = deg2rad(platAngles);
 
 % Position of platform centroid (relative to base centroid) [m]
 T = [0, 0, 0.17]';
 
-% Platform angles (x, y, z) [deg]
+% Platform angles [phi, theta, psi] [deg]
+% phi: Rotation on x-axis
+% theta: Rotation on y-axis
+% psi: Rotation on z-axis
 Pang = [0, 0, 0];
 Pang = deg2rad(Pang);
 
 % --- Calculate rod vectors ---
 % Calculate rod-platform joints (platform coords.)
+% Matrix of six column 3D vectors
 P = zeros(3, 6);
 P(1, :) = Rp .* cos(platAngles);
 P(2, :) = Rp .* sin(platAngles);
@@ -44,72 +48,65 @@ B = zeros(3, 6);
 B(1, :) = Rb .* cos(baseAngles);
 B(2, :) = Rb .* sin(baseAngles);
 
-% --- Define kinematic matrices ---
+% --- Define 3D rotation matrix ---
 ProtB = @(phi, theta, psi) ...
    [cos(psi) * cos(theta),     -sin(psi) * cos(phi) + cos(psi) * sin(theta) * sin(phi),    sin(psi) * sin(phi) + cos(psi) * sin(theta) * cos(phi); ...
     sin(psi) * cos(theta),      cos(psi) * cos(phi) + sin(psi) * sin(theta) * sin(phi),   -cos(psi) * sin(phi) + sin(psi) * sin(theta) * cos(phi); ...
    -sin(theta)           ,      cos(theta) * sin(phi)                                 ,    cos(theta) * cos(phi)];
 
 % --- Compute reverse kinematics ---
-% Leg length matrix
+% Leg length matrix (six column 3D vectors)
 L = zeros(3, 6);
 
 % Origin
 O = [0 0 0]';
 
-% Transform platform vectors
-for ii = 1:6
-   Pxyz(:, ii) = ProtB(Pang(1), Pang(2), Pang(3)) * P(:, ii); 
-end
+% Locations of servo horn joints rel. to origin
+% Matrix of six column 3D vectors
+A = zeros(3, 6);
 
-% Calculate leg vectors
-for ii = 1:6
-    L(:, ii) = T + Pxyz(:, ii) - B(:, ii);
-end
-Llen = sqrt(sum(L.^2));
-
-% Find tau vectors
-tau = zeros(3, 6);
+% Row vector of servo angles relative to horizontal
 alph = zeros(1, 6);
+
+% Row vector of servo plane angles relative to x-axis (curl around z-axis)
+% If you change the baseAngles vector, you need to recalculate these
 beta = deg2rad([90 -30 -150 90 -30 -150])';
 
+% Leg/servo angle calculation loop
 for n = 1:6
-    taun = cross(B(:, n), [0 0 1]');
-    % Convert to unit vector
-    taun /= norm(taun);
-    % Invert if odd
-    if rem(n, 2)
-        taun *= -1;
-    end
-    tau(:, n) = taun;
-end
-
-A = zeros(3, 6);
-for n = 1:6
-    % l = Llen(n);
-    l = L(:, n);
-    taun = tau(:, n);
-
-    % Calculate servo angles
     betan = beta(n);
 
+    % Rotate platform vector in global coords.
+    Pxyz(:, n) = ProtB(Pang(1), Pang(2), Pang(3)) * P(:, n); 
+
+    % Calculate leg vectors
+    l = T + Pxyz(:, n) - B(:, n);
+
+    % Calculate servo angles
     en = 2 * a * l(3);
     fn = 2 * a * (cos(betan) * l(1) + sin(betan) * l(2));
     gn = (norm(l))^2 - (s^2 - a^2);
+    
     alphn = asin(gn / sqrt(en^2 + fn^2)) - atan2(fn, en);
-    alph(n) = alphn;
 
     % Calculate servo vectors
     A(:, n) = B(:, n) + a * [cos(alphn) * cos(betan); ...
                              cos(alphn) * sin(betan); ...
                              sin(alphn)];
+
+    % Write angles and leg vectors to memory
+    alph(n) = alphn;
+    L(:, n) = l;
 end
+
+% Row vector of leg lengths
+Llen = sqrt(sum(L.^2));
 
 % Calculate rod vectors
 S = L - (A - B);
 
+% Plotting loop
 hold on;
-
 for ii = 1:6
     % Plot platform vectors
     plotVec(T, Pxyz(:, ii) + T, 'r');
@@ -117,11 +114,8 @@ for ii = 1:6
     % Plot base vectors
     plotVec(O, B(:, ii), 'g');
 
-    % Plot l-vectors
+    % Plot leg vectors
     plotVec(B(:, ii), B(:, ii) + L(:, ii), 'b');
-
-    % Plot tangent vectors
-    % plotVec(B(:, ii), B(:, ii) + tau(:, ii), 'k');
 
     % Plot servo horns
     plotVec(B(:, ii), A(:, ii), 'k');
@@ -129,7 +123,6 @@ for ii = 1:6
     % Plot rod vectors
     plotVec(A(:, ii), S(:, ii) + A(:, ii), 'k');
 end
-
 
 pbaspect([1 1 1]);
 xlabel('x [m]');
@@ -139,6 +132,7 @@ xlim([-0.2 0.2]);
 ylim([-0.2 0.2]);
 zlim([-0.1 0.2]);
 
+% Output final results
 disp('Linkage lengths:');
 for ii = 1:6
     disp(['Link ' num2str(ii) ': ' num2str(Llen(ii)) ' m']);
