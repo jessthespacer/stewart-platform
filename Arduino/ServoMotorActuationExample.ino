619a016a610d0ab679e48d83ec5e2fe65c125e8c
Jin Sing Sia <jinsing.sia@gmail.com>
2019-09-15 23:52:11 -0400

    Added Arduino code

---
+++ Arduino/ServoMotorActuationExample.ino
@@ -0,0 +1,136 @@
+/* =================================================================================================== 
+ *  This code has been provided as an example to help you get started on your project. The objective 
+ *  is to provide user input to the Arduino board and have the servo motors actuate. Several lines of 
+ *  code are accredited to the Adafruit PWM Servo Driver Library example code. To use the Adafruit 
+ *  PWM Servo Shield, be sure to add the Adafruit library to your Arduino IDE. 
+ *  (Adafruit example: File menu > Examples > Adafruit PWM Servo Drivers Library > servo)
+ *  
+ *  Add Adafruit Library: In the Arduino IDE application select: Sketch menu > Include Libraries > 
+ *  Manage Libraries. In the Library Manager window, search and install the "Adafruit PWM Servo 
+ *  Driver Library".
+ *  
+ *  NOTE: Depending on your servo motor, the pulse width min/max may be different. Adjust to match 
+ *  your servo motor.
+ =================================================================================================== */
+
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+const int SERVOMIN = 175; // 'minimum' pulse length count (out of 4096)
+const int SERVOMAX = 475; // 'maximum' pulse length count (out of 4096)
+const int SERVOMID = floor((SERVOMAX+SERVOMIN)/2); // 'mid' pulse length count (out of 4096)
+const int SERVOCHG = 50; // 'change' pulse length count
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+String valInput; // Serial input var.
+int i=0; // loop index var.
+int val[6] = {SERVOMID, SERVOMID, SERVOMID, SERVOMID, SERVOMID, SERVOMID}; // PWM var
+
+void setup() {
+  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+  Serial.setTimeout(10); // change default (1000ms) to have faster response time
+  Serial.println("Running example: Servo motor actuation using messaging");
+  pwm.begin();
+  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+
+    valInput = Serial.readString();
+    Serial.print("I received: ");
+    Serial.print(valInput);
+
+    switch (valInput[0]) {
+      // Input of "1" to "6" -> increase respective (1..6) values
+      // Input of [q,w,e,r,t,y] -> decrease respective (1..6) values
+      
+      case '1': 
+        val[0]= min( val[0]+SERVOCHG, SERVOMAX );
+        break;
+      case 'q': 
+        val[0]= max( val[0]-SERVOCHG, SERVOMIN );
+        break;
+      
+      case '2': 
+        val[1]= min( val[1]+SERVOCHG, SERVOMAX );
+        break;
+      case 'w': 
+        val[1]= max( val[1]-SERVOCHG, SERVOMIN );
+        break;
+      
+      case '3': 
+        val[2]= min( val[2]+SERVOCHG, SERVOMAX );
+        break;
+      case 'e': 
+        val[2]= max( val[2]-SERVOCHG, SERVOMIN );
+        break;
+
+      case '4':
+        val[3]= min( val[3]+SERVOCHG, SERVOMAX );
+        break;
+      case 'r':
+        val[3]= max( val[3]-SERVOCHG, SERVOMIN );
+        break;
+
+      case '5':
+        val[4]= min( val[4]+SERVOCHG, SERVOMAX );
+        break;
+      case 't':
+        val[4]= max( val[4]-SERVOCHG, SERVOMIN );
+        break;
+
+      case '6':
+        val[5]= min( val[5]+SERVOCHG, SERVOMAX );
+        break;
+      case 'y':
+        val[5]= max( val[5]-SERVOCHG, SERVOMIN );
+        break;
+
+      case '<':
+        for (i=0; i<6; i++) {
+          val[i] = SERVOMIN;
+        }
+        break;
+      case '>':
+        for (i=0; i<6; i++) {
+          val[i] = SERVOMAX;
+        }
+        break;
+      case 'm':
+        for (i=0; i<6; i++) {
+          val[i] = SERVOMID;
+        }
+        break;
+        
+        case '-':
+        for (i=0; i<6; i++) {
+          val[i] = max( val[i]-5, SERVOMIN );
+        }
+        break;
+                
+        case '+':
+        for (i=0; i<6; i++) {
+          val[i] = min( val[i]+5, SERVOMAX );
+        }
+        break;
+        
+      default: Serial.print(" No action taken");
+    } // end switch statement
+    
+    Serial.print(" Servo values = [");
+    for (i=0; i<6; i++) {
+      Serial.print(val[i]);
+      Serial.print(" ");
+    }
+    Serial.println("]");
+
+    // Update servo commands:
+    for (i=0; i<6; i++) {
+      pwm.setPWM(i+1, 0, val[i]); // added +1 to match PWM port numbering (pins 1..6 used)
+    }
+  }
+}
+
+
