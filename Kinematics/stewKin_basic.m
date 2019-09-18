cb5029c64f79c96f88a15bb083e9bea7372c93ef
Jin Sing Sia <jinsing.sia@gmail.com>
2019-09-17 22:39:36 -0400

    Added safeguard for impossible positions. Can now toggle horn calculations.

--- Kinematics/stewKin_basic.m
+++ Kinematics/stewKin_basic.m
@@ -3,6 +3,13 @@ close all; clear all;
 % https://www.instructables.com/id/Stewart-Platform/
 % https://www.xarg.org/paper/inverse-kinematics-of-a-stewart-platform/
 
+% --- Mode configuration ---
+% Plot Stewart platform in 3D?
+plotit = true;
+
+% Use servo horns?
+horn = true;
+
 % --- Stewart platform design/input parameters ---
 % Platform radius [m]
 Rp = 0.1;
@@ -11,10 +18,10 @@ Rp = 0.1;
 Rb = 0.12;
 
 % Horn length [m]
-a = 0.05;
+a = 0.008;
 
 % Rod length [m]
-s = 0.2;
+s = 0.15;
 
 % Angles of base plate joints
 % DO NOT TOUCH
@@ -27,13 +34,21 @@ platAngles = [0 60 120 180 240 300];        % Equispaced legs
 platAngles = deg2rad(platAngles);
 
 % Position of platform centroid (relative to base centroid) [m]
-T = [0, 0, 0.17]';
+T = [0, 0, 0.1485]';
 
 % Platform angles [phi, theta, psi] [deg]
 % phi: Rotation on x-axis
 % theta: Rotation on y-axis
 % psi: Rotation on z-axis
-Pang = [0, 0, 0];
+Pang = [5, 0, 0];
+
+disp('');
+disp(['Platform position: T = [' num2str(T(1)) ', ' num2str(T(2)) ', ' ...
+	num2str(T(3)) '] m']);
+disp(['Platform angle: R = [' num2str(Pang(1)) ', ' num2str(Pang(2)) ', ' ...
+	num2str(Pang(3)) '] deg']);
+disp('');
+
 Pang = deg2rad(Pang);
 
 % --- Calculate rod vectors ---
@@ -74,63 +89,75 @@ beta = deg2rad([90 -30 -150 90 -30 -150])';
 
 % Leg/servo angle calculation loop
 for n = 1:6
-    betan = beta(n);
-
     % Rotate platform vector in global coords.
     Pxyz(:, n) = ProtB(Pang(1), Pang(2), Pang(3)) * P(:, n); 
 
     % Calculate leg vectors
     l = T + Pxyz(:, n) - B(:, n);
 
-    % Calculate servo angles
-    en = 2 * a * l(3);
-    fn = 2 * a * (cos(betan) * l(1) + sin(betan) * l(2));
-    gn = (norm(l))^2 - (s^2 - a^2);
-    
-    alphn = asin(gn / sqrt(en^2 + fn^2)) - atan2(fn, en);
+    if horn
+    	betan = beta(n);
 
-    % Calculate servo vectors
-    A(:, n) = B(:, n) + a * [cos(alphn) * cos(betan); ...
-                             cos(alphn) * sin(betan); ...
-                             sin(alphn)];
+	    % Calculate servo angles
+	    en = 2 * a * l(3);
+	    fn = 2 * a * (cos(betan) * l(1) + sin(betan) * l(2));
+	    gn = (norm(l))^2 - (s^2 - a^2);
+	    
+	    alphn = asin(gn / sqrt(en^2 + fn^2)) - atan2(fn, en);
 
-    % Write angles and leg vectors to memory
-    alph(n) = alphn;
+	    if ~isreal(alphn)
+	    	disp('ERROR: This position is not possible with the current setup. Please change lengths or target position.')
+	    	disp('');
+	    	return;
+	    end
+
+	    % Calculate servo vectors
+	    A(:, n) = B(:, n) + a * [cos(alphn) * cos(betan); ...
+	                             cos(alphn) * sin(betan); ...
+	                             sin(alphn)];
+
+	    % Write angles and leg vectors to memory
+	    alph(n) = alphn;
+	end
+
     L(:, n) = l;
+end
+
+if horn
+	% Calculate rod vectors
+	S = L - (A - B);
 end
 
 % Row vector of leg lengths
 Llen = sqrt(sum(L.^2));
 
-% Calculate rod vectors
-S = L - (A - B);
-
 % Plotting loop
-hold on;
-for ii = 1:6
-    % Plot platform vectors
-    plotVec(T, Pxyz(:, ii) + T, 'r');
+if plotit
+	hold on;
+	for ii = 1:6
+	    % Plot platform vectors
+	    plotVec(T, Pxyz(:, ii) + T, 'r');
 
-    % Plot base vectors
-    plotVec(O, B(:, ii), 'g');
+	    % Plot base vectors
+	    plotVec(O, B(:, ii), 'g');
 
-    % Plot leg vectors
-    plotVec(B(:, ii), B(:, ii) + L(:, ii), 'b');
+	    % Plot leg vectors
+	    plotVec(B(:, ii), B(:, ii) + L(:, ii), 'b');
 
-    % Plot servo horns
-    plotVec(B(:, ii), A(:, ii), 'k');
+	    % Plot servo horns
+	    plotVec(B(:, ii), A(:, ii), 'k');
 
-    % Plot rod vectors
-    plotVec(A(:, ii), S(:, ii) + A(:, ii), 'k');
+	    % Plot rod vectors
+	    plotVec(A(:, ii), S(:, ii) + A(:, ii), 'k');
+	end
+	pbaspect([1 1 1]);
+	xlabel('x [m]');
+	ylabel('y [m]');
+	zlabel('z [m]');
+	xlim([-0.2 0.2]);
+	ylim([-0.2 0.2]);
+	zlim([-0.1 0.2]);
 end
-
-pbaspect([1 1 1]);
-xlabel('x [m]');
-ylabel('y [m]');
-zlabel('z [m]');
-xlim([-0.2 0.2]);
-ylim([-0.2 0.2]);
-zlim([-0.1 0.2]);
 
 % Output final results
 disp('Linkage lengths:');
@@ -140,9 +167,11 @@ end
 
 disp('');
 
-disp('Servo angles: ');
-for ii = 1:6
-    disp(['Servo ' num2str(ii) ': ' num2str(rad2deg(alph(ii))) ' deg']);
-end
+if horn
+	disp('Servo angles: ');
+	for ii = 1:6
+	    disp(['Servo ' num2str(ii) ': ' num2str(rad2deg(alph(ii))) ' deg']);
+	end
 
-disp('');
+	disp('');
+end
